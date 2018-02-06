811f0a8570eae8b23b07946b72ece96035e9b9fa
Fabien Millerand <fab@millerf.com>
2018-02-06 19:00:23 +0100

    Remove unnecessary techused2 bar
    Refactor template

--- src/app/components/subsection/subsection.component.ts
+++ src/app/components/subsection/subsection.component.ts
@@ -14,7 +14,6 @@ declare var $;
 export class SubsectionComponent implements AfterViewInit {
 
   @ViewChild('techused') techused;
-  @ViewChild('techused2') techused2;
   @ViewChild('section') section;
 
   @Input() subsection;
@@ -37,9 +36,6 @@ export class SubsectionComponent implements AfterViewInit {
     const height = Math.max(screenHeight, sectionHeight);
     if (this.techused != null) {
       this.techused.nativeElement.style.height = height + 'px';
-    }
-    if (this.techused2 != null) {
-      this.techused2.nativeElement.style.height = height + 'px';
     }
 
   }
