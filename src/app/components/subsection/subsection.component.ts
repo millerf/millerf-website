36190f161946f4dc16a323ef05c64575b326866b
Fabien Millerand <fab@mozaik.life>
2018-02-07 12:47:42 +0100

    Added vertical scrolling support

--- src/app/components/subsection/subsection.component.ts
+++ src/app/components/subsection/subsection.component.ts
@@ -1,8 +1,7 @@
 import {
-  AfterViewInit, Component, EventEmitter, HostListener, Input, OnInit, Output, ViewChild,
-  ViewChildren
+  AfterViewInit, ChangeDetectorRef, Component, EventEmitter, HostListener, Input, Output, ViewChild,
 } from '@angular/core';
-import {Project} from "../../models";
+import { Project } from "../../models";
 
 declare var $;
 
@@ -15,23 +14,49 @@ export class SubsectionComponent implements AfterViewInit {
 
   @ViewChild('techused') techused;
   @ViewChild('section') section;
+  @ViewChild('images') images;
+
 
   @Input() subsection;
   @Input() previousAnchor;
   @Input() slideLoaded;
   @Output() clickTechnology: EventEmitter<Project> = new EventEmitter();
 
-  constructor () {
+  private touchDelta = null;
+
+  constructor() {
   }
 
-  ngAfterViewInit () {
-    setTimeout( () => this.onResize(null), 500);
+  ngAfterViewInit() {
+    setTimeout(() => this.onResize(null), 500);
+
+    if (this.subsection.images.length !== 0) {
+      (<any>$)(this.images.nativeElement).mousewheel((event, delta) => {
+        (<any>$)(this.images.nativeElement)[0].scrollLeft -= (delta * 30);
+        return false;
+      })
+        .on('touchstart', (e) => {
+          this.touchDelta = this.getPosX(e);
+        })
+        .on('touchleave', (e) => {
+          this.touchDelta = null;
+        })
+        .on('touchmove', (e) => {
+          const newDelta = this.getPosX(e);
+
+          if (this.touchDelta != null) {
+            (<any>$)(this.images.nativeElement)[0].scrollLeft += this.touchDelta - newDelta;
+          }
+          this.touchDelta = newDelta;
+          return false;
+        });
+    }
   }
 
   @HostListener('window:resize', ['$event'])
   onResize(event) {
 
-    const screenHeight  = window.innerHeight;
+    const screenHeight = window.innerHeight;
     const sectionHeight = this.section.nativeElement.offsetHeight;
     const height = Math.max(screenHeight, sectionHeight);
     if (this.techused != null) {
@@ -39,4 +64,13 @@ export class SubsectionComponent implements AfterViewInit {
     }
 
   }
+
+  private getPosX(e) {
+    let touch = null;
+    if (e.originalEvent.touches) {
+      touch = e.originalEvent.touches[0];
+    }
+    return e.pageX || touch.pageX;
+  }
+
 }
